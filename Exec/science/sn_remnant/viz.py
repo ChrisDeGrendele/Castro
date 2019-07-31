13e33ee1a15bea35dc7ff596733869b00fe31040
Chris DeGrendele <christopher.degrendele@stonybrook.edu>
2019-07-31 13:52:50 -0400

    Reorganized some things and added 1d support

--- Exec/science/sn_remnant/viz.py
+++ Exec/science/sn_remnant/viz.py
@@ -1,5 +1,25 @@
 import yt
+import numpy as np
 
-ds = yt.load("plt00035")
-sl = yt.SlicePlot(ds, 2, 'temperature')
-sl.save("viz.png")
+x = np.linspace(0, 1550, 32, dtype=int)
+print(x)
+x = np.append(x,1589)
+x.astype(int)
+print(x)
+for i in x:
+    if i == 0:
+        ds = yt.load("plt00000")
+        sl = yt.SlicePlot(ds, 2, 'temperature')
+        sl.save("viz00000.png")
+    elif i < 100:
+        ds = yt.load("plt000{}".format(i))
+        sl = yt.SlicePlot(ds, 2, 'temperature')
+        sl.save("viz000{}.png".format(i))
+    elif i < 1000:
+        ds = yt.load("plt00{}".format(i))
+        sl = yt.SlicePlot(ds, 2, 'temperature')
+        sl.save("viz00{}.png".format(i))
+    elif i < 10000:
+        ds = yt.load("plt0{}".format(i))
+        sl = yt.SlicePlot(ds, 2, 'temperature')
+        sl.save("viz0{}.png".format(i))
